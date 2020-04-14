f0ad4d67dfeaefe48e1c6b451eee66d8d10cc7d5
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-04-14 14:33:03 +0100

    Evaluate asymptotic QoIs

--- examples/passiveTracer/passiveTracer.py
+++ examples/passiveTracer/passiveTracer.py
@@ -8,15 +8,44 @@ tracer source term. Neumann conditions are imposed on the
 channel walls and a Dirichlet condition is imposed on the
 inflow boundary, with the outflow boundary remaining open.
 
+The two different functional quantities of interest considered
+in [2] are evaluated on each mesh and convergence is assessed.
+
 Further details can be found in [1].
 
 [1] A. Riadh, G. Cedric, M. Jean, "TELEMAC modeling system:
     2D hydrodynamics TELEMAC-2D software release 7.0 user
     manual." Paris:  R&D, Electricite de France, p. 134
     (2014).
+
+[2] J.G. Wallwork, N. Barral, D.A. Ham, M.D. Piggott,
+    "Anisotropic Goal-Oriented Mesh Adaptation in Firedrake",
+    In: Proceedings of the 28th International Meshing
+    Roundtable (2020), DOI:10.5281/zenodo.3653101,
+    https://doi.org/10.5281/zenodo.3653101.
 """
 from thetis import *
 import numpy as np
+
+
+def bessi0(x):
+    """Modified Bessel function of the first kind. Code taken from 'Numerical recipes in C'."""
+    ax = abs(x)
+    y1 = x/3.75
+    y1 *= y1
+    expr1 = 1.0 + y1*(3.5156229 + y1*(3.0899424 + y1*(1.2067492 + y1*(0.2659732 + y1*(0.360768e-1 + y1*0.45813e-2)))))
+    y2 = 3.75/ax
+    expr2 = (exp(ax)/sqrt(ax))*(0.39894228 + y2*(0.1328592e-1 + y2*(0.225319e-2 + y2*(-0.157565e-2 + y2*(0.916281e-2 + y2*(-0.2057706e-1 + y2*(0.2635537e-1 + y2*(-0.1647633e-1 + y2*0.392377e-2))))))))
+    return conditional(le(ax, 3.75), expr1, expr2)
+
+
+def bessk0(x):
+    """Modified Bessel function of the second kind. Code taken from 'Numerical recipes in C'."""
+    y1 = x*x/4.0
+    expr1 = (-ln(x/2.0)*bessi0(x)) + (-0.57721566 + y1*(0.42278420 + y1*(0.23069756 + y1*(0.3488590e-1 + y1*(0.262698e-2 + y1*(0.10750e-3 + y1*0.74e-5))))))
+    y2 = 2.0/x
+    expr2 = (exp(-x)/sqrt(x))*(1.25331414 + y2*(-0.7832358e-1 + y2*(0.2189568e-1 + y2*(-0.1062446e-1 + y2*(0.587872e-2 + y2*(-0.251540e-2 + y2*0.53208e-3))))))
+    return conditional(ge(x, 2), expr2, expr1)
 
 
 class PassiveTracerParameters():
@@ -33,6 +62,7 @@ class PassiveTracerParameters():
         #       convergence of the uniform mesh solution to the analytical solution.
         self.source_x, self.source_y = 2.0, 5.0
         self.source_r = 0.07980 if setup == 1 else 0.07972
+        # TODO: Calibrate for this finite element space
         self.source_value = 100.0
 
         # Physical parameters
@@ -58,26 +88,40 @@ class PassiveTracerParameters():
         area_exact = pi*triple[2]**2
         scaling = 1.0 if np.allclose(area, 0.0) else area_exact/area
         scaling *= 0.5*self.source_value
-        source = self.ball(fs.mesh(), triple, scaling=scaling)
-        return interpolate(source, fs)
+        return self.ball(fs.mesh(), triple, scaling=scaling)
 
-    def quantity_of_interest(self, sol):
-        mesh = sol.function_space().mesh()
+    def quantity_of_interest_kernel(self, mesh):
         triple = (20.0, 5.0 if self.setup == 1 else 7.5, 0.5)
         area = assemble(self.ball(mesh, triple)*dx)
         area_exact = pi*triple[2]**2
         scaling = 1.0 if np.allclose(area, 0.0) else area_exact/area
-        kernel = self.ball(mesh, triple, scaling=scaling)
+        return self.ball(mesh, triple, scaling=scaling)
+
+    def quantity_of_interest(self, sol):
+        kernel = self.quantity_of_interest_kernel(sol.function_space().mesh())
         return assemble(inner(kernel, sol)*dx(degree=12))
+
+    def exact_quantity_of_interest(self, mesh):
+        x, y = SpatialCoordinate(mesh)
+        x0, y0, r = self.source_x, self.source_y, self.source_r
+        nu = self.diffusivity
+        # q = 0.01  # sediment discharge of source (kg/s)
+        q = 1
+        r = max_value(sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)), r)  # (Bessel fn explodes at (x0, y0))
+        sol = 0.5*q/(pi*nu)*exp(0.5*self.uv[0]*(x-x0)/nu)*bessk0(0.5*self.uv[0]*r/nu)
+        kernel = self.quantity_of_interest_kernel(mesh)
+        return assemble(kernel*sol*dx(degree=12))
 
 
 def solve_tracer(n, setup=1):
     mesh2d = RectangleMesh(100*2**n, 20*2**n, 50, 10)
     P1_2d = FunctionSpace(mesh2d, "CG", 1)
 
+    # Set up parameter class
     params = PassiveTracerParameters(setup=setup)
     source = params.source(P1_2d)
 
+    # Solve tracer transport problem
     solver_obj = solver2d.FlowSolver2d(mesh2d, params.bathymetry)
     options = solver_obj.options
     options.timestepper_type = 'SteadyState'
@@ -94,9 +138,10 @@ def solve_tracer(n, setup=1):
     solver_obj.bnd_functions['tracer'] = params.boundary_conditions
     solver_obj.iterate()
 
-    # Evaluates quantities of interest
+    # Evaluate quantity of interest
     sol = solver_obj.fields.tracer_2d
-    print_output("J{:d}: {:.4e}".format(setup, params.quantity_of_interest(sol)))
+    print_output("J{:d}      : {:.4e}".format(setup, params.quantity_of_interest(sol)))
+    print_output("J{:d}_exact: {:.4e}".format(setup, params.exact_quantity_of_interest(mesh2d)))
 
 
 if __name__ == "__main__":

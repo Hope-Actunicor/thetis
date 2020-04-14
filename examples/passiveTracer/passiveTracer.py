6e14398206c5133ecc15ab93c6583379edfaf859
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-04-14 13:27:20 +0100

    Note on delta function; quantities of interest

--- examples/passiveTracer/passiveTracer.py
+++ examples/passiveTracer/passiveTracer.py
@@ -16,13 +16,23 @@ Further details can be found in [1].
     (2014).
 """
 from thetis import *
+import numpy as np
 
 
 class PassiveTracerParameters():
-    def __init__(self):
+    def __init__(self, setup=1):
+        assert setup in (1, 2)
+        self.setup = setup
 
         # Parametrisation of point source
-        self.source_x, self.source_y, self.source_r = 2.0, 5.0, 0.08
+        # NOTE: Delta functions are not in H1 and hence do not live in the FunctionSpaces we seek
+        #       to use. The idea here is to approximate the delta function using a disc with a very
+        #       small radius. This works well for practical applications, but is not quite right for
+        #       analytical test cases such as this. As such, we have calibrated the disc radius so
+        #       that solving on a sequence of increasingly refined uniform meshes leads to
+        #       convergence of the uniform mesh solution to the analytical solution.
+        self.source_x, self.source_y = 2.0, 5.0
+        self.source_r = 0.07980 if setup == 1 else 0.07972
         self.source_value = 100.0
 
         # Physical parameters
@@ -37,23 +47,35 @@ class PassiveTracerParameters():
         outflow = {'open': None}
         self.boundary_conditions = {1: dirichlet, 2: outflow, 3: neumann, 4: neumann}
 
-    def ball(self, mesh, scaling=1.0, eps=1.0e-10):
+    def ball(self, mesh, triple, scaling=1.0, eps=1.0e-10):
         x, y = SpatialCoordinate(mesh)
-        expr = lt((x-self.source_x)**2 + (y-self.source_y)**2, self.source_r**2 + eps)
+        expr = lt((x-triple[0])**2 + (y-triple[1])**2, triple[2]**2 + eps)
         return conditional(expr, scaling, 0.0)
 
     def source(self, fs):
-        nrm = assemble(self.ball(fs.mesh())*dx)
-        scaling = 0.5*self.source_value*pi*self.source_r**2/nrm
-        source = self.ball(fs.mesh(), scaling=scaling)
+        triple = (self.source_x, self.source_y, self.source_r)
+        area = assemble(self.ball(fs.mesh(), triple)*dx)
+        area_exact = pi*triple[2]**2
+        scaling = 1.0 if np.allclose(area, 0.0) else area_exact/area
+        scaling *= 0.5*self.source_value
+        source = self.ball(fs.mesh(), triple, scaling=scaling)
         return interpolate(source, fs)
 
+    def quantity_of_interest(self, sol):
+        mesh = sol.function_space().mesh()
+        triple = (20.0, 5.0 if self.setup == 1 else 7.5, 0.5)
+        area = assemble(self.ball(mesh, triple)*dx)
+        area_exact = pi*triple[2]**2
+        scaling = 1.0 if np.allclose(area, 0.0) else area_exact/area
+        kernel = self.ball(mesh, triple, scaling=scaling)
+        return assemble(inner(kernel, sol)*dx(degree=12))
 
-def solve_tracer(n):
+
+def solve_tracer(n, setup=1):
     mesh2d = RectangleMesh(100*2**n, 20*2**n, 50, 10)
     P1_2d = FunctionSpace(mesh2d, "CG", 1)
 
-    params = PassiveTracerParameters()
+    params = PassiveTracerParameters(setup=setup)
     source = params.source(P1_2d)
 
     solver_obj = solver2d.FlowSolver2d(mesh2d, params.bathymetry)
@@ -62,10 +84,6 @@ def solve_tracer(n):
     options.timestep = 20.0
     options.simulation_end_time = 18.0
     options.simulation_export_time = 18.0
-    # options.timestepper_type = 'CrankNicolson'
-    # options.timestep = 0.5
-    # options.simulation_end_time = 100.0
-    # options.simulation_export_time = 10.0
     options.fields_to_export = ['tracer_2d']
     options.solve_tracer = True
     options.use_lax_friedrichs_tracer = True
@@ -76,6 +94,12 @@ def solve_tracer(n):
     solver_obj.bnd_functions['tracer'] = params.boundary_conditions
     solver_obj.iterate()
 
+    # Evaluates quantities of interest
+    sol = solver_obj.fields.tracer_2d
+    print_output("J{:d}: {:.4e}".format(setup, params.quantity_of_interest(sol)))
+
 
 if __name__ == "__main__":
-    solve_tracer(2)
+    refinement_level = 2
+    for setup in (1, 2):
+        solve_tracer(refinement_level, setup=setup)
